bbf0d60eae57c4a43ee01c6fe729555ec99abebc
cantellim <mattcantelli@gmail.com>
2016-10-14 22:17:34 -0400

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -438,7 +438,7 @@ var resizePizzas = function(size) {
       console.log("bug in sizeSwitcher");
   }
 
-var randomPizzas = document.getElementByClass(".randomPizzaContainer");
+var randomPizzas = document.querySelectorAll(".randomPizzaContainer");
 
 for (var i = 0; i < randomPizzas.length; i++) {
   randomPizzas[i].style.width = newWidth + "%";
